6e0112fdc885a6cbf18d08ca0626b6cb409b9936
didier <didaxlm@gmail.com>
2020-01-27 16:01:49 +0100

    initializ spring

---
+++ BackEnd/src/main/java/fr/didier/giveApp/GiveAppApplication.java
@@ -0,0 +1,13 @@
+package fr.didier.giveApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiveAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiveAppApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/fr/didier/giveApp/GiveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.didier.giveApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiveAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
