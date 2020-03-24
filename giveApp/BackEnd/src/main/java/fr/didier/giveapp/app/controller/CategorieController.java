308727a89a7b310c54cb28c4f363d3915d31fc97
didaxlm <didaxlm@gmail.com>
2020-03-24 09:52:35 +0100

    MaJ controller categorie et faq

--- giveApp/BackEnd/src/main/java/fr/didier/giveapp/app/controller/CategorieController.java
+++ giveApp/BackEnd/src/main/java/fr/didier/giveapp/app/controller/CategorieController.java
@@ -1,5 +1,26 @@
 package fr.didier.giveapp.app.controller;
 
-public class CategorieController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.didier.giveapp.app.model.Categorie;
+import fr.didier.giveapp.app.repository.CategorieRepository;
+
+@RestController
+@RequestMapping("/categories")
+public class CategorieController 
+{
+	@Autowired
+	private CategorieRepository categorieDepot;
+	
+	@GetMapping
+	public List<Categorie> afficherListeCategorie()
+	{
+		List<Categorie> allCategorie = categorieDepot.findAll();
+		return allCategorie;
+	}
 }
